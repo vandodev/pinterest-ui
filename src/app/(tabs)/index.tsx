7f4df77a072f8fd00257b5f076bd71325cd65b09
vandodev <vandoaparecido@hotmail.com>
2024-02-29 07:20:25 -0300

    :red_circle: :o: :black_circle: Criando masonry layout

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import {View, StyleSheet} from "react-native";
 import { theme } from "../../theme";
-import { Filters } from "@/components/Filters";
 
-import {FILTERS} from "@/utils/filters"
-import { useState } from "react";
+import { POSTS } from "@/utils/posts"
+import { FILTERS } from "@/utils/filters"
+
+import { Posts } from "@/components/Posts"
+import { Filters } from "@/components/Filters"
 
 export default function Home(){
     const [filter, setFilter] = useState(FILTERS[0])
 
     return(
         <View style={styles.container}>
-            <Filters filters={FILTERS} filter={filter} onChange={setFilter}/>            
+            <Filters filters={FILTERS} filter={filter} onChange={setFilter}/> 
+            <Posts posts={POSTS} />           
         </View>
     )
 }
